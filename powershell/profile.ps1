bf40d28e0e62d1eb9b317c0b444164ca85e0453b
Kidus Adugna <kidusadugna@gmail.com>
2019-09-25 16:59:25 +0300

    powershell: add sudo and aliases

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -6,6 +6,22 @@ function prompt {
     $time = get-date -Format "HH:mm"
     write-host "$pwd" -ForegroundColor Blue
     write-host "$time" -NoNewline -ForegroundColor White
-    write-host " $" -NoNewline -ForegroundColor Green
+    write-host " $" -NoNewline -ForegroundColor Cyan
     return " "
 }
+
+New-Alias gpg D:\K1DV5\DevPrograms\gnupg\bin\gpg
+# Set-Alias curl D:\K1DV5\DevPrograms\Neovim\bin\curl
+
+# hehe, sudo!
+function sudo {
+    $executable, $arguments = $args.Split(" ", 2)
+    if ($arguments) {
+        Start-Process $executable -args $arguments -Verb runAs
+    } else {
+        Start-Process $executable -Verb runAs
+    }
+}
+
+New-Alias chrome "C:\Program Files (x86)\Google\Chrome\Application\chrome"
+New-Alias 7z D:\K1DV5\DevPrograms\7-z\7z
