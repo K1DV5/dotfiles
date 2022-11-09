667ac302fdcfc8b8f00b6e7b3eb8bc7b37fa0471
K1DV5 <kidusadugna@gmail.com>
2022-11-09 20:56:19 +0300

    update for new cmp version

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -102,7 +102,7 @@ vim.fn.sign_define("DiagnosticSignInfo", {text = '', texthl = "DiagnosticSign
 vim.fn.sign_define("DiagnosticSignHint", {text = '', texthl = "DiagnosticSignHint"})
 
 -- enable snippets support on client
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- setup language servers
 local servers = {
