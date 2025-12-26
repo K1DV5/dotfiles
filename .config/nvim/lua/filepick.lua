a9100fb0523966e4d36f050bc6d30c7bc46e8035
K1DV5 <kidusadugna@gmail.com>
2025-12-26 13:52:49 +0100

    full file system ops with filepick

--- .config/nvim/lua/filepick.lua
+++ .config/nvim/lua/filepick.lua
@@ -1,11 +1,59 @@
+-- $ nvim -l %f
+
 local M = {}
 
 local cmd = "git ls-files -c -o --exclude-standard --deduplicate"
-local cmd_non_git = "find . -type f"
+local ignore = '\\( -path ./node_modules -o -path ./.git \\) -prune -o '
+local find_f = "-type f -printf '%P\\n'"
+local find_d = "-type d -printf '%P/\\n'"
+local cmd_non_git = "find . " .. ignore .. find_f
+local cmd_non_git_all = "find . -mindepth 1 " .. ignore .. find_f .. " -or " .. find_d
+local cmd_non_git_dirs = 'find . -mindepth 1 ' .. ignore .. find_d
+
+local manifest = {
+  FilePick = {
+    cmd = cmd,
+    key = '-',
+    handle = function(arg) vim.cmd.edit({args = {arg}}) end,
+  },
+  FilePickCreate = {
+    cmd = cmd_non_git_dirs,
+    key = '1-',
+    handle = function(dir)
+      local path = vim.fn.input('In: ', dir)
+      vim.fn.mkdir(vim.fn.fnamemodify(path, ':h'), "p")
+      vim.cmd.edit({args = {path}}) 
+    end
+  },
+  FilePickRename = {
+    cmd = cmd_non_git_all,
+    key = '2-',
+    handle = function(path)
+      vim.fn.rename(path, vim.fn.input('To: ', path))
+    end
+  },
+  FilePickCopy = {
+    cmd = cmd_non_git_all,
+    key = '3-',
+    handle = function(path)
+      vim.uv.fs_copyfile(path, vim.fn.input('To: ', path))
+    end
+  },
+  FilePickDelete = {
+    cmd = cmd_non_git_all,
+    key = '0-',
+    handle = function(path)
+      if vim.fn.confirm("Delete " .. path .. "?", "&Yes\n&No") == 1 then
+        vim.fn.delete(path, "rf")
+      end
+    end
+  },
+}
 
 local function get_file_completion(arg_lead, cmdline, cur_pos)
-  local cmdout = io.popen(cmd):read("*a")
-  if cmdout == "" then
+  local command = (manifest[vim.split(cmdline, " ")[1]] or {cmd = cmd_non_git}).cmd
+  local cmdout = io.popen(command):read("*a")
+  if cmdout == "" and command ~= cmd_non_git then
     cmdout = io.popen(cmd_non_git):read("*a")
   end
   return vim.split(vim.trim(cmdout), "\n")
@@ -13,24 +61,25 @@ end
 
 local function cmd_handler(opts)
   local arg = vim.trim(opts.args)
-  if arg ~= "" then
-    vim.cmd.edit({args = {arg}})
+  local conf = manifest[opts.name]
+  if conf then
+    conf.handle(arg)
   end
 end
 
-local assist_cmds = {FilePick = true}
-
 function M.blink_check_assist(ctx)
-  return assist_cmds[vim.split(ctx.line, " ")[1]] or false
+  return manifest[vim.split(ctx.line, " ")[1]] ~= nil
 end
 
 function M.setup()
-  vim.api.nvim_create_user_command(
-    "FilePick",
-    cmd_handler,
-    { complete = get_file_completion, nargs = '*', force = true }
-  )
-  vim.keymap.set('n', '-', function () vim.api.nvim_input(':FilePick ') end)
+  for cmd, conf in pairs(manifest) do
+    vim.api.nvim_create_user_command(
+      cmd,
+      cmd_handler,
+      { complete = get_file_completion, nargs = '*', force = true }
+    )
+    vim.keymap.set('n', conf.key, function () vim.api.nvim_input(':' .. cmd .. ' ') end)
+  end
 end
 
 return M
